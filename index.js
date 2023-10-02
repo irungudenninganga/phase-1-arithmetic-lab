62cd0eab6cc8d51a2dc20ad8bdf8b25d969115b2
+dennisngangairungu <+>
2023-10-02 20:43:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1=31
+let num2=2
+let num3=22
+let num4=9
+
+let multiply=num1*num2
+console.log(multiply)
+let random =Math.floor(Math.random()*20)+1
+console.log(random)
+let mod=num3 %num4
+console.log(mod)
+let max=Math.max(1,4,7,10,15,20)
+console.log(max)
